b2ebdd3b5f28a74fc723cc36f4a1a2d69d7ee623
Gabriel Demarqui <123519736+Gabriel-Demarqui@users.noreply.github.com>
2025-08-24 19:40:10 -0300

    meu portifolio

---
+++ index.js
@@ -0,0 +1,19 @@
+const menutoggle = document.querySelector(".menutoggle")
+
+const navbar = document.querySelector(".navbar")
+
+menutoggle.addEventListener("click", () => 
+navbar.classList.toggle("active")
+) 
+
+const sections = document.querySelectorAll("section");
+   const observer = new IntersectionObserver((entries) => {
+     entries.forEach(entry => {
+       if (entry.isIntersecting) {
+         entry.target.classList.add("show");   // aparece
+       } else {
+         entry.target.classList.remove("show"); // some de novo
+       }
+     });
+   }, { threshold: 0.2 }); // ativa quando 20% do elemento estiver visível
+sections.forEach(sec => observer.observe(sec));
