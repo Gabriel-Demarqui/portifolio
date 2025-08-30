06a4319602572b53e42b1806e0c66061e1672b46
Gabriel-Demarqui <gabrielcolaco063@gmail.com>
2025-08-30 18:05:31 -0300

    styloCelular

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ menutoggle.addEventListener("click", () =>
 navbar.classList.toggle("active")
 ) 
 
-const sections = document.querySelectorAll("section");
-   const observer = new IntersectionObserver((entries) => {
-     entries.forEach(entry => {
-       if (entry.isIntersecting) {
-         entry.target.classList.add("show");   // aparece
-       } else {
-         entry.target.classList.remove("show"); // some de novo
-       }
-     });
-   }, { threshold: 0.2 }); // ativa quando 20% do elemento estiver visível
-sections.forEach(sec => observer.observe(sec));
+// const sections = document.querySelectorAll("section");
+   //const observer = new IntersectionObserver((entries) => {
+    // entries.forEach(entry => {
+    //   if (entry.isIntersecting) {
+     //    entry.target.classList.add("show");   // aparece
+     //  } else {
+      //   entry.target.classList.remove("show"); // some de novo
+      // }
+     //});
+   //}, { threshold: 0.2 }); // ativa quando 20% do elemento estiver visível
+//sections.forEach(sec => observer.observe(sec));
+
